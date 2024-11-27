50f42e531d1bf59ccc64257eb711192ffbd1d808
abdullah956 <abdullaharshed956@gmail.com>
2024-11-27 19:04:57 +0500

    users

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
 
 def home(request):
-    return render(request, 'home.html')
+    users = User.objects.all()  
+    return render(request, 'home.html', {'users': users})
 
 def logout_view(request):
     logout(request)
